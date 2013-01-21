3a40269fbd1acbf5d97cfdcb345e5a9a0b1753c1
Ian Wizard <1.am.w1z4rd@gmail.com>
2013-01-21 00:58:39 -0800

    Fixed config parsing bug, and added true concurrency.

--- CFBalancer.py
+++ CFBalancer.py
@@ -1,6 +1,6 @@
 import argparse;
 from hashlib import sha256;
-from multiprocessing import cpu_count;
+from multiprocessing import cpu_count, Process;
 from os import getloadavg;
 from socket import socket, AF_INET, SOCK_DGRAM;
 
@@ -8,6 +8,7 @@ from twisted.internet.protocol import DatagramProtocol;
 from twisted.internet import reactor, task;
 
 # Globals
+txbytes = None;
 netload = None;
 
 # A global dict to contain the config.
@@ -49,8 +50,10 @@ def parse_config(filename):
 
 	return config;
 
-def get_netload(old_netload):
+def update_netload(old_txbytes):
 	"""Returns the current txbytes for the interface, read from /proc"""
+	global txbytes, netload;
+
 	try:
 		f = open('/proc/net/dev', 'r');
 		lines = f.readlines();
@@ -60,35 +63,33 @@ def get_netload(old_netload):
 
 	for line in lines:
 		if (line[:line.find(':')].strip() == config['NETLOAD_INTERFACE']):
-			return int(line[line.find(':'):].split()[9]) - old_netload;
+			txbytes = int(line[line.find(':'):].split()[9]);
+			netload = txbytes - old_txbytes;
+
+def send_heartbeats(hosts):
+	heartbeat = str(config['NODE_DL_CNAME'] + ',' + str(getloadavg()[0] / config['CPU_CORES']) + ',' + str(netload));
+	for host in hosts:
+		socket(AF_INET, SOCK_DGRAM).sendto(sha256(heartbeat + config['SHARED_SECRET']).hexdigest() + heartbeat, (host, int(config['HEARTBEAT_PORT'])));
+
+def update_server_table():
+	update_netload(txbytes);
+
+	if (config['SEND_HEARTBEATS']):
+		print(server_table);
+		Process(target = send_heartbeats, args = [server_table.keys()]).start();
+
+	for ip in server_table.keys():		# For every ip in the server table...
+		if (server_table[ip]):				# If the ip is still in the server_table...
+			if (server_table[ip][0] > 10):  # If it's been more than 10 seconds since we got a heartbeat...
+				del server_table[ip];			# ...delete the host from server_table.
+			else:							# Otherwise...
+				server_table[ip][0] += 1;	# ...increase the count since we last heard from them.
 
 class Heartbeat(DatagramProtocol):
 	"""Monitors for heartbeats, and updates the server_table with each one."""
 	def datagramReceived(self, data, (host, port)):
 		if (data[:64] == sha256(data[64:] + config['SHARED_SECRET']).hexdigest()):	# If the security hash matches...
 			server_table[host] = [0, data[64:]]		# ...update the server table with the heartbeat data.
-
-def send_heartbeats():
-	global netload;
-
-	print(server_table);
-
-	# Build the heartbeat
-	netload = get_netload(netload);
-	if (config['SEND_HEARTBEATS']):
-		heartbeat = str(config['NODE_DL_CNAME'] + ',' + str(getloadavg()[0] / config['CPU_CORES']) + ',' + str(netload));
-
-	for ip in server_table.copy():				# For every server ip we know about...
-		# ...send our heartbeat to the host...
-		if (heartbeat):
-			socket(AF_INET, SOCK_DGRAM).sendto(sha256(heartbeat + config['SHARED_SECRET']).hexdigest() + heartbeat, (ip, int(config['HEARTBEAT_PORT'])));
-
-		# ...and update the server table.
-		if (server_table[ip]):				# If the ip is in the server_table still...
-			if (server_table[ip][0] > 10):  # If it's been more than 10 seconds since we got a heartbeat...
-				del server_table[ip];					# ...delete the host from server_table.
-			else:
-				server_table[ip][0] += 1;	# Otherwise, increase the count since we last heard from them.
 
 def main():
 	argparser = argparse.ArgumentParser(description = 'CodeFire load balancer daemon.');
@@ -101,8 +102,12 @@ def main():
 		config['CONFIG_FILE'] = str(parse_config('/etc/codefire')['DATASTORE'] + config['CONFIG_FILE']);
 
 	try:
-		for ip in config['CODEFIRE_WEB_IPS']:
-			server_table[ip] = None;
+		if type(config['CODEFIRE_WEB_IPS']) == list:
+			for ip in config['CODEFIRE_WEB_IPS']:
+				if (ip):
+					server_table[ip] = None;
+		elif type(config['CODEFIRE_WEB_IPS']) == str:
+			server_table[config['CODEFIRE_WEB_IPS']] = None;
 	except:
 		exit(-1);
 
@@ -112,7 +117,7 @@ def main():
 
 	# Now start the real work.
 	reactor.listenUDP(int(config['HEARTBEAT_PORT']), Heartbeat());
-	task.LoopingCall(send_heartbeats).start(int(config['HEARTBEAT_INTERVAL']) / 1000.0);
+	task.LoopingCall(update_server_table).start(int(config['HEARTBEAT_INTERVAL']) / 1000.0);
 	reactor.run()
 
 if __name__ == '__main__':
