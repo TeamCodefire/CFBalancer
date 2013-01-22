c2d03106290a5c76e8006f37ade0dc35b72f9650
Ian Wizard <1.am.w1z4rd@gmail.com>
2013-01-22 01:42:08 -0800

    Fixed concurrency, and streamlined code slightly.

--- CFBalancer.py
+++ CFBalancer.py
@@ -1,6 +1,6 @@
 import argparse;
+import multiprocessing;
 from hashlib import sha256;
-from multiprocessing import cpu_count, Process;
 from os import getloadavg;
 from socket import socket, AF_INET, SOCK_DGRAM;
 
@@ -8,13 +8,13 @@ from twisted.internet.protocol import DatagramProtocol;
 from twisted.internet import reactor, task;
 
 # Globals
-txbytes = None;
-heartbeat_process = None;
+heartbeat_pipe = None;
 
 # A global dict to contain the config.
 config = {
 	'CONFIG_FILE': '/configs/loadbalancer/lb.conf',
-	'CPU_CORES': cpu_count(),
+	'CPU_CORES': multiprocessing.cpu_count(),
+	'DAEMON': False,
 	'SEND_HEARTBEATS': True
 }
 
@@ -50,33 +50,46 @@ def parse_config(filename):
 
 	return config;
 
-def get_netload(old_txbytes):
-	"""Returns the current txbytes for the interface, read from /proc"""
-	global txbytes, netload;
-
-	try:
-		f = open('/proc/net/dev', 'r');
-		lines = f.readlines();
-		f.close();
-	except:
-		return;
-
-	for line in lines:
-		if (line[:line.find(':')].strip() == config['NETLOAD_INTERFACE']):
-			txbytes = int(line[line.find(':'):].split()[9]);
-			return txbytes - old_txbytes;
-
-def send_heartbeats(*args, **kwargs):
+def send_heartbeats(pipe):
 	"""Send out heartbeats, to all hosts."""
-	heartbeat = str(config['NODE_DL_CNAME'] + ',' + str(getloadavg()[0] / config['CPU_CORES']) + ',' + str(kwargs['netload']));
-	for host in args:
-		socket(AF_INET, SOCK_DGRAM).sendto(sha256(heartbeat + config['SHARED_SECRET']).hexdigest() + heartbeat, (host, int(config['HEARTBEAT_PORT'])));
+	from time import sleep;
+
+	netload = 0;
+	txbytes = 0;
+
+	while (True):
+		sleep(1);
+		try:
+			f = open('/proc/net/dev', 'r');
+			lines = f.readlines();
+			f.close();
+		except:
+			return;
+
+		for line in lines:
+			if (line[:line.find(':')].strip() == config['NETLOAD_INTERFACE']):
+				netload = int(line[line.find(':'):].split()[9]) - txbytes;
+				txbytes += netload;
+
+		if (pipe.poll()):
+			# Build the heartbeat.
+			heartbeat = str(config['NODE_DL_CNAME'] + ',' + str(getloadavg()[0] / config['CPU_CORES']) + ',' + str(netload));
+
+			# And send it to every host in the list.
+			try:
+				for host in pipe.recv():
+					socket(AF_INET, SOCK_DGRAM).sendto(sha256(heartbeat + config['SHARED_SECRET']).hexdigest() + heartbeat, (host, int(config['HEARTBEAT_PORT'])));
+			except:
+				pass;
 
 def update_server_table():
 	"""Update the server_table, and send out heartbeats."""
-	print(server_table);
+	if (not config['DAEMON']):
+		print(server_table);
+
 	if (config['SEND_HEARTBEATS']):
-		Process(target = send_heartbeats, args = server_table.keys(), kwargs = {'netload': get_netload(txbytes)}).start();
+		# Send hosts list to the heartbeat subprocess.
+		heartbeat_pipe.send(server_table.keys());
 
 	for ip in server_table.keys():		# For every ip in the server table...
 		if (server_table[ip]):				# If the ip is still in the server_table...
@@ -86,22 +99,27 @@ def update_server_table():
 				server_table[ip][0] += 1;	# ...increase the count since we last heard from them.
 
 class Heartbeat(DatagramProtocol):
-	"""Monitors for heartbeats, and updates the server_table with each one."""
+	"""Heartbeat Handler: Monitors for heartbeats, and updates the server_table when one is received."""
 	def datagramReceived(self, data, (host, port)):
 		if (data[:64] == sha256(data[64:] + config['SHARED_SECRET']).hexdigest()):	# If the security hash matches...
 			server_table[host] = [0, data[64:]]		# ...update the server table with the heartbeat data.
 
 def main():
 	"""Initialize everything, and start the event loop."""
-	argparser = argparse.ArgumentParser(description = 'CodeFire load balancer daemon.');
+	global heartbeat_pipe;
+
+	# Handle the argument parser.
+	argparser = argparse.ArgumentParser(description = 'Team CodeFire load reporting daemon.');
 	argparser.add_argument('--config');
 	args = argparser.parse_args();
 
+	# Load the configs.
 	if (args.config):
 		config.update(parse_config(args.config));
 	else:
 		config['CONFIG_FILE'] = str(parse_config('/etc/codefire')['DATASTORE'] + config['CONFIG_FILE']);
 
+	# Add the default hosts.
 	try:
 		if type(config['CODEFIRE_WEB_IPS']) == list:
 			for ip in config['CODEFIRE_WEB_IPS']:
@@ -112,12 +130,14 @@ def main():
 	except:
 		exit(-1);
 
-	# Free up some ram.
-	global cpu_count;
-	del cpu_count;
+	# Start the heartbeat process...
+	rxpipe, heartbeat_pipe = multiprocessing.Pipe(False);
+	multiprocessing.Process(target = send_heartbeats, args = [rxpipe]).start();
 
-	# Now start the real work.
-	p = Process(target = send_heartbeats);
+	# Free up some ram.
+	del rxpipe;
+
+	# Register the listeners and start the event loop.
 	reactor.listenUDP(int(config['HEARTBEAT_PORT']), Heartbeat());
 	task.LoopingCall(update_server_table).start(int(config['HEARTBEAT_INTERVAL']) / 1000.0);
 	reactor.run()
